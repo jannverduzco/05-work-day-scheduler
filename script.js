a71728c73860cf3adfa83572e3411506a2119bb2
verja <janneth_verduzco@live.com>
2020-11-11 23:46:17 -0600

    added local storate code

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
 $(document).ready(function () {
 
-
     // VARIABLES ----------------------------------------------
-    var userTask = "";
-
+    
+   
     // current date display
     var date = moment().format("dddd MMMM Do, YYYY")
     $("#currentDay").text(date);
 
-
     // LISTENERS ------------------------------------------------
 
- 
+
     // FUNCTIONS ------------------------------------------------
 
     $(".description").each(function () {
         //convert data-hou string to number & declares the variable
         var dataHour = parseInt($(this).attr("data-hour"));
+        
         // declares hour from moment.js format to currentTime
         var currentTime = moment().format("H");
         // compares moment.js current time hour to data-hour string to set past
@@ -38,12 +37,20 @@ $(document).ready(function () {
 
 
       
-    // $(this).siblings(".description").val()
+    // save added tasks
     $(".saveBtn").on("click", function () {
-        userTask.text
+    var userTask = $(this).siblings(".description").val()
+    var descriptionEl = $(this).attr("id")
+    // console.log(descriptionEl)
+    localStorage.setItem(descriptionEl , userTask);
+    console.log(userTask)
     });
 
+    for (let i = 9; i <= 17 ; i++) {
+        $("#" + i + "des").val(localStorage.getItem(i))
+        // console.log(i)
+    }
 
-
+   
 
 }); 
