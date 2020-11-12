4da5e7d6da3ffe27c7f5efa59630c1459dbfff3b
verja <janneth_verduzco@live.com>
2020-11-11 18:42:18 -0600

    added color code task description section per time of day

--- script.js
+++ script.js
@@ -1,9 +1,50 @@
-// VARIABLES -------------------
-var 
+$(document).ready(function () {
 
-// LISTENERS -------------------
-$("text-block").on("click", function() {
 
-})
+    // VARIABLES -------------------
+    var userTask = "";
 
-// FUNCTIONS -------------------
+    // current date display
+    var date = moment().format("dddd MMMM Do, YYYY")
+    $("#currentDay").text(date);
+
+
+    // LISTENERS -------------------
+
+    // time-block task
+    // $(this).siblings(".description").val()
+    $(".saveBtn").on("click", function () {
+        userTask.text
+    });
+
+    // FUNCTIONS -------------------
+
+    $(".description").each(function () {
+        //convert data-hou string to number & declares the variable
+        var dataHour = parseInt($(this).attr("data-hour"));
+        // declares hour from moment.js format to currentTime
+        var currentTime = moment().format("H");
+        // compares moment.js current time hour to data-hour string to set past
+        if (currentTime > dataHour) {
+            $(this).addClass("past")
+        }
+         // compares moment.js current time hour to data-hour string to set present &removes previous set class
+        else if (currentTime == dataHour) {
+            $(this).addClass("present")
+            $(this).removeClass("past")
+        }
+        // add furute class for what else applies to & removes previous set classes
+        else {
+            $(this).addClass("future")
+            $(this).removeClass("past")
+            $(this).removeClass("present")
+        }
+
+
+
+
+    })
+
+
+
+}); 
