d4c327b7e3e04e898b9ade59b7f5fddb919d115a
verja <janneth_verduzco@live.com>
2020-11-12 18:28:01 -0600

    added getItem to reflect task stored

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
 
     // VARIABLES ----------------------------------------------
-    
-   
+
+
     // current date display
     var date = moment().format("dddd MMMM Do, YYYY")
     $("#currentDay").text(date);
@@ -14,15 +14,15 @@ $(document).ready(function () {
 
     $(".description").each(function () {
         //convert data-hou string to number & declares the variable
-        var dataHour = parseInt($(this).attr("data-hour"));
-        
+        var dataHour = parseInt($(this).attr("id"));
+
         // declares hour from moment.js format to currentTime
         var currentTime = moment().format("H");
         // compares moment.js current time hour to data-hour string to set past
         if (currentTime > dataHour) {
             $(this).addClass("past")
         }
-         // compares moment.js current time hour to data-hour string to set present &removes previous set class
+        // compares moment.js current time hour to data-hour string to set present &removes previous set class
         else if (currentTime == dataHour) {
             $(this).addClass("present")
             $(this).removeClass("past")
@@ -36,27 +36,39 @@ $(document).ready(function () {
     })
 
 
-      
-    // save added tasks
-    $(".saveBtn").on("click", function () { 
-    // setting a variable for the value of the description
-    var userTask = $(this).siblings(".description").val()
-    //varibale set for key target
-    var descriptionEl = $(this).attr("id")
-    // set in my local storage for key, value
-    localStorage.setItem(descriptionEl , userTask);
-    console.log(userTask)
+
+    // Save added task into local storage
+    $(".saveBtn").on("click", function () {
+        // setting a variable for the value of the description
+        var userTask = $(this).siblings(".description").val()
+        //varibale set key target
+        var descriptionEl = $(this).attr("id")
+        // set in my local storage for key, value
+        localStorage.setItem(descriptionEl, userTask);
+        console.log(userTask)
     });
-    
-    
-    for (let i = 9; i <= 17 ; i++) {
-        var myData = localStorage.getItem(i + "btn")
-        // $("#" + i + "des").val(localStorage.getItem(i ))
-        console.log(myData)
-    }
-    
+
+    // Retrieve added tast from local Storage
 
 
+    $("#9").val(localStorage.getItem("9btn"))
+    $("#10").val(localStorage.getItem("10btn"))
+    $("#11").val(localStorage.getItem("11btn"))
+    $("#12").val(localStorage.getItem("12btn"))
+    $("#13").val(localStorage.getItem("13btn"))
+    $("#14").val(localStorage.getItem("14btn"))
+    $("#15").val(localStorage.getItem("15btn"))
+    $("#16").val(localStorage.getItem("16btn"))
+    $("#17").val(localStorage.getItem("17btn"))
    
+    // for (let i = 9; i <= 17 ; i++) {
+    // var myData = localStorage.getItem()
+    //     // $("#" + i + "").val(localStorage.getItem(i ))
+    // console.log(myData)
+    // }
+
+
+
+
 
 }); 
